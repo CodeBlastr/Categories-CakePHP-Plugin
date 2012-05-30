a06c0c721c349d9bd7d81d23eaed84aef95acd9d
Anthony Costa <anthony@razorit.com>
2012-05-30 18:12:24 -0400

    Added Categorizable Behavior and Test to Categories Plugin

---
+++ Model/Behavior/CategorizableBehavior.php
@@ -0,0 +1,115 @@
+<?php
+App::uses('ModelBehavior', 'Model');
+App::uses('Category', 'Categories.Model');
+
+/**
+ * Categorizable Behavior class file.
+ * 
+ * Usage is :
+ * Attach behavior to a model, and when you save if valid category data exists the object will be categorized automatically. 
+ *
+ * @filesource
+ * @author			Richard Kersey
+ * @copyright       RazorIT LLC
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link            https://github.com/zuha/Categories-Zuha-Cakephp-Plugin
+ */
+class CategorizableBehavior extends ModelBehavior {
+
+/**
+ * Behavior settings
+ * 
+ * @access public
+ * @var array
+ */
+	public $settings = array();
+
+/**
+ * The full results of Model::find() that are modified and saved
+ * as a new copy.
+ *
+ * @access public
+ * @var array
+ */
+	public $record = array();
+
+/**
+ * Default values for settings.
+ *
+ * - recursive: whether to copy hasMany and hasOne records
+ * - habtm: whether to copy hasAndBelongsToMany associations
+ * - stripFields: fields to strip during copy process
+ * - ignore: aliases of any associations that should be ignored, using dot (.) notation.
+ * will look in the $this->contain array.
+ *
+ * @access private
+ * @var array
+ */
+    protected $defaults = array(
+		'modelAlias' => null, // changed to $Model->alias in setup()
+		//'foreignKeyName' => null,
+		);
+
+
+/**
+ * Configuration method.
+ *
+ * @param object $Model Model object
+ * @param array $config Config array
+ * @access public
+ * @return boolean
+ */
+    public function setup($Model, $config = array()) {
+    	$this->settings = array_merge($this->defaults, $config);
+		$this->modelName = !empty($this->settings['modelAlias']) ? $this->settings['modelAlias'] : $Model->alias;
+		//don't think this is necessary, but will save it for future reference in the case that there is a different primary key than id
+		//$this->foreignKey =  !empty($this->settings['foreignKeyName']) ? $this->settings['foreignKeyName'] : $Model->primaryKey;
+		$this->Category = new Category;
+    	return true;
+	}
+	
+
+/**
+ * Before save method.
+ *
+ * Remove category data, and pass to the after save function for manual entry.
+ *
+ * @param object $Model model object
+ * @param mixed $id String or integer model ID
+ * @access public
+ * @return boolean
+ */
+	public function beforeSave($Model, $created) {
+		if (isset($Model->data['Category']['Category'])) {
+			$this->data = $Model->data;
+			unset($Model->data['Category']['Category']);
+		}
+		return true;
+	}
+	
+
+/**
+ * After save method.
+ *
+ * If the data array object contains a value $data['Draft']['status'] = 1, then save draft.
+ *
+ * @param object $Model model object
+ * @param mixed $id String or integer model ID
+ * @access public
+ * @return boolean
+ */
+	public function afterSave($Model, $created) {
+		# this is how the categories data should look when coming in.
+		if (isset($this->data['Category']['Category'])) {
+			$categorized = array($this->modelName => array('id' => array($Model->id)));
+			$categorized['Category']['id'][] = $this->data['Category']['Category'];
+			try {
+				$this->Category->categorized($categorized, $Model->alias);
+			} catch (Exception $e) {
+				throw new Exception ($e->getMessage());
+			}
+		}
+
+	}
+	
+}
---
+++ Test/Case/Model/Behavior/CategorizableBehaviorTest.php
@@ -0,0 +1,79 @@
+<?php
+App::uses('CategorizableBehavior', 'Categories.Model/Behavior');
+
+
+/**
+ * CategorizableBehavior Test Case
+ *
+ */
+class CategorizableBehaviorTestCase extends CakeTestCase {
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.categories.article',
+		'plugin.categories.categorized',
+		//'plugin.categories.category',
+		);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Categorizable = new CategorizableBehavior();
+		$this->Model = Classregistry::init('Article'); // not tied to an actual model file
+		$this->Model->Behaviors->attach('Categories.Categorizable');
+		//$this->Category = ClassRegistry::init('Categories.Category');
+		$this->Categorized = ClassRegistry::init('Categories.Categorized');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Categorizable);
+		unset($this->Category);
+		unset($this->Categorized);
+
+		parent::tearDown();
+	}
+	
+
+/**
+ * Test behavior instance
+ *
+ * @return void
+ */
+	public function testBehaviorInstance() {
+		$this->assertTrue(is_a($this->Model->Behaviors->Categorizable, 'CategorizableBehavior'));
+	}
+	
+
+	
+/**
+ * testAfterSave method
+ *
+ * @return void
+ */
+	public function testAfterSave() {	
+		$article['Article']['title'] = "My Test Article";
+		$article['Category']['Category'] = "category-1";
+		$this->Model->save($article);
+		$result = $this->Categorized->find('first', array('conditions' => array('Categorized.model' => 'Article', 'Categorized.foreign_key' => $this->Model->id)));
+		$this->assertNotEmpty($result);
+	}
+	
+}
