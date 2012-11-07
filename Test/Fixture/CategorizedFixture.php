74185810e2532c0d0d35ef77c1c5757207d620db
Richard Kersey <richard@razorit.com>
2012-11-07 17:32:05 -0500

    Added Test Fixtures

--- Test/Fixture/CategorizedFixture.php
+++ Test/Fixture/CategorizedFixture.php
@@ -23,30 +23,14 @@ class CategorizedFixture extends CakeTestFixture {
  * @var string $name
  */
 	public $name = 'Categorized';
-
+    
 /**
- * Table
- *
- * @var array $table
+ * Import
+ * 
+ * @var array 
  */
-	public $table = 'categorized';
+	public $import = array('config' => 'Categories.Categorized', 'uses' => 'categorized');
 
-/**
- * Fields
- *
- * @var array $fields
- */
-	public $fields = array(
-		'id' => array('type'=>'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
-		'category_id' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 36),
-		'foreign_key' => array('type'=>'string', 'null' => true, 'default' => NULL, 'length' => 36),
-		'model' => array('type'=>'string', 'null' => false, 'default' => NULL),
-		'record_count' => array('type'=>'integer', 'null' => true, 'default' => 0),
-		'created' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
-		'modified' => array('type'=>'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'UNIQUE_CATEGORY_CONTENT' => array('column' => array('category_id', 'foreign_key', 'model'), 'unique' => 1)));
 
 /**
  * Records
---
+++ Test/Fixture/CategorizedOptionFixture.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * CategorizedOptionFixture
+ *
+ */
+class CategorizedOptionFixture extends CakeTestFixture {
+
+/**
+ * Name
+ *
+ * @var string $name
+ */
+	public $name = 'CategorizedOption';
+	
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('config' => 'Categories.CategorizedOption');
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array();
+}
+
