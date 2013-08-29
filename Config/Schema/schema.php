5d4933b34a0970d67abdc3403d847c487ed90cc5
Buildrr LLC <richard@buildrr.com>
2013-08-29 20:41:15 +0000

    Fixed schema to work with updates ui

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -28,7 +28,7 @@ class CategoriesSchema extends CakeSchema {
 		'parent_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'lft' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
 		'rght' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'slug' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'type' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50, 'collate' => 'utf8_general_ci', 'comment' => 'Category, Attribute Group, Attribute Type, Option Group, Option Type, ', 'charset' => 'utf8'),
