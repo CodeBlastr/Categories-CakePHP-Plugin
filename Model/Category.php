b010e7cdad6def62976894eda08f24699b61075c
Buildrr LLC <richard@buildrr.com>
2013-11-18 01:20:12 +0000

    Added a use case for categories to show on the categories dashboard.

--- Model/Category.php
+++ Model/Category.php
@@ -333,8 +333,7 @@ class Category extends CategoriesAppModel {
 		foreach ($return as $key => $model) {
 			$model = ZuhaInflector::pluginize($model) ? ZuhaInflector::pluginize($model).'.'.$model : $model;
 			$Model = ClassRegistry::init($model);
-
-			if (!is_array($Model->actsAs) || !array_search('Categories.Categorizable', $Model->actsAs)) {
+			if (!is_array($Model->actsAs) || (!array_search('Categories.Categorizable', $Model->actsAs) && empty($Model->actsAs['Categories.Categorizable']))) {
 				// remove models which aren't categorizable
 				unset($return[$key]);
 			}
