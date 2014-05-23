9085ff684de3d12177b140c47ae3e22be81ebf2d
Joel Byrnes <joel@razorit.com>
2014-05-23 14:37:40 -0400

    formatting & phpDocs

--- View/Helper/CategoryHelper.php
+++ View/Helper/CategoryHelper.php
@@ -1,27 +1,33 @@
 <?php
 
 class CategoryHelper extends AppHelper {
-	
+
+/**
+ * @param View $View
+ * @param array $settings
+ */
 	public function __construct(View $View, $settings = array()) {
 		parent::__construct($View, $settings);
-		
+
 		if ($this->request->isPost()) {
 			$this->_handlePost();
 		}
 	}
 
 /**
- * Find method
+ * @param string $type
+ * @param array $params
+ * @return array
  */
- 	public function find($type = 'first', $params = array()) {
+	public function find($type = 'first', $params = array()) {
 		$this->Category = ClassRegistry::init('Categories.Category');
- 		return $this->Category->find($type, $params);
- 	}
+		return $this->Category->find($type, $params);
+	}
 
 /**
- * Find threaded
+ * @return array
  */
-	public function findThreaded (){
+	public function findThreaded() {
 		$this->Category = ClassRegistry::init('Categories.Category');
 		$threaded = $this->Category->find('threaded');
 		$models = Set::extract('/Category/model', $this->Category->find('all', array('group' => array('Category.model'), 'fields' => array('Category.model'))));
@@ -29,52 +35,61 @@ class CategoryHelper extends AppHelper {
 			foreach ($threaded as $thread) {
 				if ($thread['Category']['model'] == $model) {
 					$categories[$model][] = $thread;
-					$options[$model] = $this->Category->generateTreeList(array('Category.model' => $model), null, null, '--');
+					//$options[$model] = $this->Category->generateTreeList(array('Category.model' => $model), null, null, '--');
 				}
 			}
 		}
-		return $categories; 
+		return $categories;
 	}
 
 /**
- * Load data
+ * @param array $options
+ * @return array
  */
 	public function loadData($options = array()) {
 		$this->Category = ClassRegistry::init('Categories.Category');
-		// $joins = array(
-			           // array('table'=>'categorized', 
-			                 // 'alias' => 'Categorized',
-			                 // 'type'=>'left',
-			                 // 'conditions'=> array(
-			                 	// 'Categorized.foreign_key = Classified.id'
-			           // )),
-			           // array('table'=>'categories', 
-			                 // 'alias' => 'Category',
-			                 // 'type'=>'left',
-			                 // 'conditions'=> array(
-			                 	// 'Category.id = Categorized.category_id'
-					   // ))
-			         // );
+//		$joins = array(
+//			array('table' => 'categorized',
+//				'alias' => 'Categorized',
+//				'type' => 'left',
+//				'conditions' => array(
+//					'Categorized.foreign_key = Classified.id'
+//				)),
+//			array('table' => 'categories',
+//				'alias' => 'Category',
+//				'type' => 'left',
+//				'conditions' => array(
+//					'Category.id = Categorized.category_id'
+//				))
+//		);
 		$defaults = array('contain' => 'Categorized');
 		$options = Set::merge($options, $defaults);
 		$data = $this->Category->find('all', $options);
 		return $data;
 	}
-	
-	
-	
+
+/**
+ * @return array
+ */
 	public function displayList() {
 		$Category = ClassRegistry::init('Categories.Category');
 		return $Category->find('list', array('conditions' => array('Category.parent_id' => '')));
 	}
-	
+
+/**
+ * @param char $id
+ * @return type
+ */
 	public function displayItems($id) {
 		$Category = ClassRegistry::init('Categories.Category');
 		return( $Category->view($id) );
 	}
-	
-	
-	
+
+/**
+ * @param array $categories
+ * @param array $options
+ * @return string
+ */
 	public function display($categories = array(), $options = array()) {
 		$output = '';
 		switch ($options['type']) {
@@ -88,7 +103,11 @@ class CategoryHelper extends AppHelper {
 		}
 		return $output;
 	}
-	
+
+/**
+ * @param array $array
+ * @return string
+ */
 	protected function _recursiveUl($array) {
 		$output = '';
 		if (!empty($array)) {
@@ -103,17 +122,22 @@ class CategoryHelper extends AppHelper {
 		}
 		return $output;
 	}
-	
+
+/**
+ * @param array $array
+ * @param array $options
+ * @return string
+ */
 	protected function _selectForm($array, $options) {
 		$output = '';
 		if (!empty($array)) {
 			$output .= '<form method="post">';
-			$output .= '<input type="hidden" name="data[Category][model]" value="'.$options['model'].'" />';
-			$output .= '<input type="hidden" name="data[Category][foreignKey]" value="'.$options['foreignKey'].'" />';
+			$output .= '<input type="hidden" name="data[Category][model]" value="' . $options['model'] . '" />';
+			$output .= '<input type="hidden" name="data[Category][foreignKey]" value="' . $options['foreignKey'] . '" />';
 			$output .= '<select name="data[Category][Category][0]">';
 			$output .= '<option value="">- none -</option>';
 			foreach ($array as $item) {
-				$output .= '<option value="'.$item['Category']['id'].'">';
+				$output .= '<option value="' . $item['Category']['id'] . '">';
 				$output .= $item['Category']['name'];
 				$output .= '</option>';
 			}
@@ -123,7 +147,10 @@ class CategoryHelper extends AppHelper {
 		}
 		return $output;
 	}
-	
+
+/**
+ * @throws Exception
+ */
 	protected function _handlePost() {
 		if (!empty($this->request->data['Category'])) {
 			$categorized = array($this->request->data['Category']['model'] => array('id' => array($this->request->data['Category']['foreignKey'])));
@@ -136,11 +163,11 @@ class CategoryHelper extends AppHelper {
 			}
 			$this->Category = ClassRegistry::init('Categories.Category');
 			try {
-        		$this->Category->categorized($categorized, $this->request->data['Category']['model']);
+				$this->Category->categorized($categorized, $this->request->data['Category']['model']);
 			} catch (Exception $e) {
-				throw new Exception ($e->getMessage());
+				throw new Exception($e->getMessage());
 			}
 		}
 	}
-	
+
 }
